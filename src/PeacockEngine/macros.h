68d3cbd9d09e40ae09a2037ba52fc44f8adb8ecf
Henry Peacock <henster23@hotmail.co.uk>
2019-10-11 14:57:21 +0100

    Added macro header file

---
+++ src/PeacockEngine/macros.h
@@ -0,0 +1,2 @@
+#define shared std::shared_ptr
+#define weak std::weak_ptr
--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "../PeacockEngine/macros.h"
 
 int main()
 {
 	std::cout<<"Fortnite Bad"<<std::endl<<"Minecraft Good"<<std::endl;
 	std::cin.get();
-	
+
 	return 0;
 }
